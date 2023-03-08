9188db5db39f9dda28d97b92f5aa1dbf352ab035
unknown <saurab.singh2048@gmail.com>
2023-03-08 19:19:34 +0545

    Generate Question model via artisan model generator

--- database/migrations/2023_03_08_093432_create_questions_table.php
+++ database/migrations/2023_03_08_093432_create_questions_table.php
@@ -15,7 +15,17 @@ return new class extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
+            $table->string('titile');
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->unsignedInteger('views')->default(0);
+            $table->unsignedInteger('answers')->default(0);
+            $table->integer('votes')->default(0);
+            $table->unsignedInteger('best_answer_id')->nullable();
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
