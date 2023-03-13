58beadb7376c33e3675b4e6804a77acbada2ba4b
unknown <saurab.singh2048@gmail.com>
2023-03-13 21:52:58 +0545

    edit and update the question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -70,7 +70,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        //
+        return view("questions.edit",compact('question'));
     }
 
     /**
@@ -80,9 +80,10 @@ class QuestionsController extends Controller
      * @param  \App\Models\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $question->update($request->only('title','body'));
+        return redirect('/questions')->with('success', "Your question has been updated.");
     }
 
     /**
