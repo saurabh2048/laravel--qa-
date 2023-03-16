ea18596cc266dd44c5b4afc8409731dadafbcfe9
unknown <saurab.singh2048@gmail.com>
2023-03-16 21:14:44 +0545

    created model, migration for Answers

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -33,7 +33,7 @@ class Question extends Model
 
     public function getStatusAttribute()
     {
-        if($this->answers > 0){
+        if($this->answers_count > 0){
             if($this->best_answer_id){
                 return "answered-accepted";
             }
@@ -45,6 +45,11 @@ class Question extends Model
     public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->line($this->body);
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
     }
 
     /*
